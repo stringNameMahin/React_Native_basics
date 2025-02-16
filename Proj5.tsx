48c828c95929b18864b5f7929d9ef0351caf510f
stringNameMahin <dhokemahin@gmail.com>
2025-02-17 02:20:00 +0530

    propswithchildren

--- Proj5.tsx
+++ Proj5.tsx
@@ -1,13 +1,24 @@
 // Dice Roll App
 
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { ImageSourcePropType, StyleSheet, Text, View } from 'react-native'
+import React, { PropsWithChildren } from 'react'
+
+import DiceOne from './Img/One.png'
+import DiceTwo from './Img/Two.png'
+import DiceThree from './ImgThree.png'
+import DiceFour from './Img/Four.png'
+import DiceFive from './Img/Five.png'
+import DiceSix from './Img/Six.png'
+
+type DiceProps = PropsWithChildren<{
+  imageUrl: ImageSourcePropType
+}>
 
 export default function Proj5() {
   return (
-    <View>
-      <Text>Proj5</Text>
-    </View>
+    <>
+    
+    </>
   )
 }
 
--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
 // This file is use to declare module data type e.g png -- this is a typescript practice that allows ts to know what extension is going to be inserted to the tsx or more.
 
 declare module '*.png'
+declare module '*.jpg'
